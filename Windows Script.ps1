9e3076d07da2efa27e2c5fe001cddafa909c8fc6
Trolip <kwright6200@gmail.com>
2024-11-14 17:34:08 -0600

    Update Windows Script.ps1

--- Windows Script.ps1
+++ Windows Script.ps1
@@ -2,11 +2,12 @@
 
 #Enables script running
 #Set-ExecutionPolicy Unrestricted
+#y
 
 #Download the script in Windows / change the name to your current user
 #Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Trolip/CP-17-Bubble-Guppies-Windows-Script.ps1/refs/heads/main/Windows%20Script.ps1" -OutFile C:\Users\ashepard\Downloads\Script.ps1
 
-#run script
+#run script / change name to current user
 #C:\Users\ashepard\Downloads\Script.ps1
 
 #find .mp3 files
@@ -28,6 +29,7 @@ if(get-localuser -name Guest) {
                                                             #SECPOL
 #audit policy / fix
 auditpol /restore /file:audit.csv
+AuditPol /set /subcategory:"Object Access" /success:enable /failure:enable
 auditpol /get /category:*
 read-host "Check the audits to make sure they are all set. Then, press Enter to continue..."
 
@@ -36,8 +38,16 @@ secedit /import /cfg:Password Policies.inf
 read-host "Check the audits to make sure they are all set. Then, press Enter to continue..."
 
                                                             #FIREWALL
-# Windows Defender firewall in advanced
+# enable Windows Defender firewall in advanced and logging
 Set-NetFirewallProfile -Profile Domain, Private, Public -DefaultInboundAction Block -DefaultOutboundAction Allow
+Set-NetFirewallProfile -Profile Domain,Public,Private -LogFileName "C:\FirewallLog.log" -LogAllowed True -LogBlocked True -LogIgnored True
+
+#UPnP is not enabled
+Get-NetFirewallRule -DisplayGroup "Network Discovery" | Where-Object { $_.Enabled -eq "False" }
+
+#Windows Defender no longer runs in passive mode'
+Set-MpPreference -DisableRealtimeMonitoring $false
+
 Get-NetFirewallProfile
 read-host "Check the firewall domain, private, and public for blocked inbound action and allowed outbound action. Then, press Enter to continue..."
 
@@ -50,10 +60,18 @@ Start-Service -Name WinDefend
 Set-Service -Name WinDefend -StartupType Automatic
 
                                               #SERVICES, PROCESSES, AND OPTIONAL FEATURES
+#stop process
+#Get-Process
+
+
 #disable FTP
 Stop-Service -Name ftpsvc
 Set-Service -Name ftpsvc -StartupType Disabled
 Disable-WindowsOptionalFeature -Online -FeatureName "IIS-FTPServer" -Remove
+
+#Windows Automatic Updates are enabled'
+Set-Service -Name wuauserv -StartupType Automatic
+Start-Service -Name wuauserv
 
 #Disable AutoPlay for all drives
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\AutoplayHandlers" -Name "DisableAutoplay" -Value 1
@@ -61,16 +79,35 @@ Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer
 #Disable AutoPlay for CD/DVD and other media types
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name "NoDriveTypeAutoRun" -Value 0x000000FF
 
+#The system requires approval from an administrator before running unknown software and Users cannot bypass attempts to run unknown software
+Set-MpPreference -EnableSmartScreen $true
+
 #Windows SmartScreen configured to warn or block
 Set-MpPreference -EnableSmartScreen $true -SmartScreenAppInstallControl "Block"
 
 #World Wide Web Publishing service has been stopped and disabled
 Stop-Service -Name w3svc
-                                                           
+
+#disable Bitlocker
+Disable-BitLocker -MountPoint "C:"
+
+#Windows Event Log service is running
+Start-Service -Name eventlog
+
+#Removed Telnet from Windows features
+Disable-WindowsOptionalFeature -Online -FeatureName TelnetClient
+
+#stop services
+Get-Service
+Set-Service -Name "nc.exe" -StartupType "Disabled"
+
+#Windows Update services are running (WaaSMedicSvc)
+Start-Service -Name WaaSMedicSvc
                                                             #USERS
-#create users
+#password for users
 $password = ConvertTo-SecureString "20-R1p-CdR-24" -AsPlainText -Force
                                                               #EDIT
+#create users
 get-localuser
 $names = @()
 while ($true) {
@@ -134,7 +171,7 @@ while ($true) {
   }
   else if ($input -eq "1") {
     $username = read-host "Enter the name of a user who will be removed from a group."
-    $group = read-host "Enter the group name that they'll be leaving."
+    $group = read-host "Enter the group name they'll be leaving."
     Remove-LocalGroupMember -Group $group -Member $username
   }
  else if ($input -eq "2") {
@@ -143,6 +180,8 @@ while ($true) {
   }
 }
 
+#limit local use of a blank password to console only
+Set-LocalUser -Name "UserName" -PasswordNeverExpires $false
                                                                 #FIX
 #see if users have a good password and set their password to a predetermined value
 #foreach (get-localuser -Group "Users") {
@@ -150,8 +189,7 @@ while ($true) {
 
                                                               
 
-#limit local use of a blank password to console only
-Set-LocalUser -Name "UserName" -PasswordNeverExpires $false
+                                                                                  #REGISTRY
 
 #DO not allow anonymous enumeration of SAM accounts
 Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Lsa" -Name "RestrictAnonymous" -Value 1
@@ -161,15 +199,6 @@ Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Lsa" -Name "Restr
 
 #RDP Security Layer set to SSL
 Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Terminal Server" -Name "SecurityLayer" -Value 1
-
-#log allowed, blocked, and ignored for advanced windows firewall
-Set-NetFirewallProfile -Profile Domain,Public,Private -LogFileName "C:\FirewallLog.log" -LogAllowed True -LogBlocked True -LogIgnored True
-
-#disable Bitlocker
-Disable-BitLocker -MountPoint "C:"
-
-#Find-Script
-find-script
 
 #passwords stored using reversible encryption
 Set-ADDomain -Identity "DomainName" -AllowReversiblePasswordEncryption $false
@@ -183,9 +212,6 @@ Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Lsa" -Name "Restr
 #Elevation prompts run on the secure desktop'
 Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System" -Name "PromptOnSecureDesktop" -Value 1
 
-#Audit object access [Success/Failure]
-AuditPol /set /subcategory:"Object Access" /success:enable /failure:enable
-
 #Users cannot add or logon with Microsoft accounts'
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\System" -Name "NoConnectedUser" -Value 1
 
@@ -198,14 +224,8 @@ Set-ItemProperty -Path "HKCU:\Software\Mozilla\Firefox" -Name "FirefoxProfile" -
 #Core dumps are disabled on system crash'
 Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\CrashControl" -Name "CrashDumpEnabled" -Value 0
 
-#Windows Defender no longer runs in passive mode'
-Set-MpPreference -DisableRealtimeMonitoring $false
-
 #Only administrators can install printer drivers'
 Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Print\Environments\Windows x64\Drivers" -Name "RestrictDriverInstall" -Value 1
-
-#The system requires approval from an administrator before running unknown software and Users cannot bypass attempts to run unknown software
-Set-MpPreference -EnableSmartScreen $true
 
 #File/registry write failures are virtualized to per-user locations'
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\System" -Name "EnableVirtualization" -Value 1
@@ -213,24 +233,11 @@ Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies
 #App notifications do not show on the lock screen
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Notifications\Settings" -Name "NOC_GLOBAL_SETTING_ALLOW_TOASTS_ABOVE_LOCK" -Value 0
 
-#Windows Automatic Updates are enabled'
-Set-Service -Name wuauserv -StartupType Automatic
-Start-Service -Name wuauserv
-
 #User access to the Store application is prohibited'
 Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\WindowsStore" -Name "RemoveWindowsStore" -Value 1
 
 #UEFI Secure Boot is enabled'
 Get-WmiObject -Class Win32_BIOS | Select-Object SecureBoot
-
-#Windows Update services are running (WaaSMedicSvc)
-Start-Service -Name WaaSMedicSvc
-
-#Windows Event Log service is running'
-Start-Service -Name eventlog
-
-#Removed Telnet from Windows features'
-Disable-WindowsOptionalFeature -Online -FeatureName TelnetClient
 
 #RDP connections require SSL'
 Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Terminal Server" -Name "SecurityLayer" -Value 1
@@ -244,22 +251,12 @@ Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Lsa" -Name "FIPSA
 #check for hidden network shares
 Get-WmiObject -Class Win32_Share | Where-Object { $_.Type -eq 0 }
 
-#UPnP is not enabled
-Get-NetFirewallRule -DisplayGroup "Network Discovery" | Where-Object { $_.Enabled -eq "False" }
-
-#check for shell backdoor in windows/systemresources
-Get-ChildItem -Path "C:\Windows\System32" -Recurse -Include *backdoor*
-
-#stop services
-Get-Service
-Set-Service -Name "nc.exe" -StartupType "Disabled"
-
 #stop process
 Get-Process
 Stop-Process -Name "notepad"
 Stop-Process -Id 1234
 
-  #UPDATES
+                                                                              #UPDATES
 #update firefox
 if(Get-ChildItem "C:\Program Files\Mozilla Firefox" -Recurse -Filter firefox.exe -or Get-ChildItem "C:\Program Files (x86)\Mozilla Firefox" -Recurse -Filter firefox.exe
 ){
@@ -297,10 +294,16 @@ winget uninstall "Npcap"
 Install-Module -Name PSWindowsUpdate -Force -Scope CurrentUser
 Install-WindowsUpdate -AcceptAll -AutoReboot
 
+#Find-Script
+find-script
+
+#check for shell backdoor in windows/systemresources
+Get-ChildItem -Path "C:\Windows\System32" -Recurse -Include *backdoor*
+
 #Restart the computer
 Restart-computer
 
-write-host "SCript Finished"
+write-host "Script Finished"
 
 #Disables script running
 Set-ExecutionPolicy Restricted
