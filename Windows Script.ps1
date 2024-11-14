a441bf68af85d7e2d38883b9e8b51befe3cec1a5
Trolip <kwright6200@gmail.com>
2024-11-13 21:46:48 -0600

    Update Windows Script.ps1

--- Windows Script.ps1
+++ Windows Script.ps1
@@ -28,9 +28,9 @@ read-host
 
 #secedit policy/fix
 secedit /import /cfg:Password Policies.inf
-
 write-host "Check the audits to make sure they are all set. Then, press Enter to continue..."
 read-host
+
 # Windows Defender firewall in advanced
 Set-NetFirewallProfile -Profile Domain, Private, Public -DefaultInboundAction Block -DefaultOutboundAction Allow
 Get-NetFirewallProfile
@@ -42,7 +42,6 @@ Stop-Service -Name ftpsvc
 Set-Service -Name ftpsvc -StartupType Disabled
 Disable-WindowsOptionalFeature -Online -FeatureName "IIS-FTPServer" -Remove
 
-                                                                  #todo
 #create users/change name and replicate the second line for each user
 $Password = ConvertTo-SecureString "20-R1p-CdR-24" -AsPlainText -Force
 New-LocalUser -Name "JohnDoe" -Password $Password 
@@ -57,9 +56,11 @@ Remove-localuser -Name "JohnDoe"
 Add-LocalGroupMember -Group "Administrators" -Member "JohnDoe"
 Remove-LocalGroupMember -Group "Administrators" -Member "JohnDoe"
 
+                                                                #FIX
 #see if users have a good password and set their password to a predetermined value
-for (get-localuser - Group "Users") {
-  if(
+#for (get-localuser -Group "Users") {
+  #if(
+
 #Enable Windows firewall and cloud-delivered protection
 Set-MpPreference -DisableRealtimeMonitoring $false
 Set-MpPreference -DisableAntiSpyware $false
@@ -68,9 +69,9 @@ Start-Service -Name WinDefend
 Set-Service -Name WinDefend -StartupType Automatic
 
 #disable auto-play
-#Disable AutoPlay for all drives
+  #Disable AutoPlay for all drives
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\AutoplayHandlers" -Name "DisableAutoplay" -Value 1
-#Disable AutoPlay for CD/DVD and other media types
+  #Disable AutoPlay for CD/DVD and other media types
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" -Name "NoDriveTypeAutoRun" -Value 0x000000FF
 
 #Windows SmartScreen configured to warn or block
@@ -84,69 +85,139 @@ if(Get-ChildItem "C:\Program Files\Mozilla Firefox" -Recurse -Filter firefox.exe
 ){
  winget upgrade "Mozilla Firefox"
 }
+
 #update chrome
 if(Get-ChildItem "C:\Program Files\Google Chrome" -Recurse -Filter Chrome.exe -or Get-ChildItem "C:\Program Files (x86)\Google Chrome" -Recurse -Filter chrome.exe
 ){
   winget upgrade "Google Chrome"
 }
+
 #update mozilla thunderbird
 if(Get-ChildItem "C:\Program Files\Mozilla Thunderbird" -Recurse -Filter thunderbird.exe -or Get-ChildItem "C:\Program Files (x86)\Mozilla Thunderbird" -Recurse -Filter thunderbird.exe
 ){
   winget upgrade "Mozilla Thunderbird"
 }
-#updates all apps
-winget upgrade --all
 
 #notepad updated
 if(Get-ChildItem "C:\Program Files\Notepad" -Recurse -Filter notepad.exe -or Get-ChildItem "C:\Program Files (x86)\Notepad" -Recurse -Filter notepad.exe
 ){
   winget upgrade "Notepad"
 }
+
+#updates all apps
+winget upgrade --all
+
 #removed Wireshark
 winget uninstall "Wireshark"
 
 #removed npcap
 winget uninstall "Npcap"
 
-
+                                                          #CHECK
+                                                          
 #windows update majority
 Install-Module -Name PSWindowsUpdate -Force -Scope CurrentUser
 Install-WindowsUpdate -AcceptAll -AutoReboot
+
 #limit local use of a blank password to console only
+Set-LocalUser -Name "UserName" -PasswordNeverExpires $false
+
 #DO not allow anonymous enumeration of SAM accounts
+Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Lsa" -Name "RestrictAnonymous" -Value 1
+
 #restrict global object creation
+Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Lsa" -Name "RestrictGlobalObjects" -Value 1
+
 #RDP Security Layer set to SSL
+Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Terminal Server" -Name "SecurityLayer" -Value 1
+
 #log allowed, blocked, and ignored for advanced windows firewall
-#Disable-Bitlocker
+Set-NetFirewallProfile -Profile Domain,Public,Private -LogFileName "C:\FirewallLog.log" -LogAllowed True -LogBlocked True -LogIgnored True
+
+#disable Bitlocker
+Disable-BitLocker -MountPoint "C:"
+
 #Find-Script
+find-script
+
 #passwords stored using reversible encryption
+Set-ADDomain -Identity "DomainName" -AllowReversiblePasswordEncryption $false
+
 #LAN Manager only sends NTLMv2 responses'
+Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Lsa" -Name "LmCompatibilityLevel" -Value 5
+
 #NULL session fallback is prohibited'
+Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Lsa" -Name "RestrictAnonymous" -Value 1
+
 #Elevation prompts run on the secure desktop'
-#Audit object access [Success/Failure]'
+Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\System" -Name "PromptOnSecureDesktop" -Value 1
+
+#Audit object access [Success/Failure]
+AuditPol /set /subcategory:"Object Access" /success:enable /failure:enable
+
 #Users cannot add or logon with Microsoft accounts'
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\System" -Name "NoConnectedUser" -Value 1
+
 #Nobody can access Credential Manager as a trusted caller'
+Set-ItemProperty -Path "HKLM:\Software\Microsoft\Windows\CurrentVersion\Policies\CredUI" -Name "DisableDomainCreds" -Value 1
+
 #Firefox profiles and sync disabled'
+Set-ItemProperty -Path "HKCU:\Software\Mozilla\Firefox" -Name "FirefoxProfile" -Value ""
+
 #Core dumps are disabled on system crash'
+Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\CrashControl" -Name "CrashDumpEnabled" -Value 0
+
 #Windows Defender no longer runs in passive mode'
+Set-MpPreference -DisableRealtimeMonitoring $false
+
 #Only administrators can install printer drivers'
-#The system requires approval from an administrator before running unknown software (Defender Smartscreen)'
-#Users cannot bypass attempts to run unknown software (Defender Smartscreen)'
+Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Print\Environments\Windows x64\Drivers" -Name "RestrictDriverInstall" -Value 1
+
+#The system requires approval from an administrator before running unknown software and Users cannot bypass attempts to run unknown software
+Set-MpPreference -EnableSmartScreen $true
+
 #File/registry write failures are virtualized to per-user locations'
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\System" -Name "EnableVirtualization" -Value 1
+
 #App notifications do not show on the lock screen
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Notifications\Settings" -Name "NOC_GLOBAL_SETTING_ALLOW_TOASTS_ABOVE_LOCK" -Value 0
+
 #Windows Automatic Updates are enabled'
+Set-Service -Name wuauserv -StartupType Automatic
+Start-Service -Name wuauserv
+
 #User access to the Store application is prohibited'
+Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\WindowsStore" -Name "RemoveWindowsStore" -Value 1
+
 #UEFI Secure Boot is enabled'
-#Windows Update services are running - wuauserv and WaaSMedicSvc'
+Get-WmiObject -Class Win32_BIOS | Select-Object SecureBoot
+
+#Windows Update services are running (WaaSMedicSvc)
+Start-Service -Name WaaSMedicSvc
+
 #Windows Event Log service is running'
+Start-Service -Name eventlog
+
 #Removed Telnet from Windows features'
+Disable-WindowsOptionalFeature -Online -FeatureName TelnetClient
+
 #RDP connections require SSL'
+Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Terminal Server" -Name "SecurityLayer" -Value 1
+
 #RDP requires a secure RPC connection'
+Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Terminal Server" -Name "fDenyTSConnections" -Value 1
+
 #The system is configured to use FIPS 140 compliant cryptographic algorithms'
-#File and registry write failures are virtualized to per-user locations'
+Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\Lsa" -Name "FIPSAlgorithmPolicy" -Value 1
+
 #check for hidden network shares
-#UPnP is not enabled / Get-NetFirewallRule -DisplayGroup "Network Discovery" | Where-Object { $_.Enabled -eq "False"}'
+Get-WmiObject -Class Win32_Share | Where-Object { $_.Type -eq 0 }
+
+#UPnP is not enabled
+Get-NetFirewallRule -DisplayGroup "Network Discovery" | Where-Object { $_.Enabled -eq "False" }
+
 #check for shell backdoor in windows/systemresources
+Get-ChildItem -Path "C:\Windows\System32" -Recurse -Include *backdoor*
 
 #stop services
 Get-Service
@@ -157,18 +228,11 @@ Get-Process
 Stop-Process -Name "notepad"
 Stop-Process -Id 1234
 
-Using IIS Manager (GUI)
-If you prefer a GUI to manage the service:
-
-Press Windows + R, type inetmgr, and press Enter to open IIS Manager.
-In the left pane, expand the Server node.
-
 #Restart the computer
 Restart-computer
 
 #Pause and unpause the script
 write-host "Script Finished"
-
 
 #Disables script running
 Set-ExecutionPolicy Restricted
